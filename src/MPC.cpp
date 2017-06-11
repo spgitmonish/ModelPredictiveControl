8b011c2474bc0d90639687f85bc9eecf217231a9
spgitmonish <monish.sunku@walletbuddies.co>
2017-06-11 10:53:37 -0600

    Changing cost factors for 60mph
    Work in progress but car makes a lap around.

--- src/MPC.cpp
+++ src/MPC.cpp
@@ -22,7 +22,7 @@ double dt = 0.05;
 const double Lf = 2.67;
 
 // Reference velocity for the cost function calculation
-double ref_v = 40;
+double ref_v = 60;
 
 // The solver takes all the state variables and actuator variables in single vector.
 // Establish when one variable starts and ends within the single vector.
@@ -64,14 +64,14 @@ class FG_eval
     for (int t = 0; t < N; t++)
     {
       fg[0] += CppAD::pow(vars[cte_start + t], 2);
-      fg[0] += CppAD::pow(vars[epsi_start + t], 2);
+      fg[0] += 2.0 * CppAD::pow(vars[epsi_start + t], 2);
       fg[0] += CppAD::pow(vars[v_start + t] - ref_v, 2);
     }
 
     // Minimize the magnitude of actuators and prevent sharp spikes i.e. change rate
     for (int t = 0; t < N - 1; t++)
     {
-      fg[0] += CppAD::pow(vars[delta_start + t], 2);
+      fg[0] += 2.0 * CppAD::pow(vars[delta_start + t], 2);
       fg[0] += CppAD::pow(vars[a_start + t], 2);
     }
 
@@ -81,7 +81,7 @@ class FG_eval
     //       between the values at time 't+1' and 't'
     for (int t = 0; t < N - 2; t++)
     {
-      fg[0] += 15000 * (CppAD::pow(vars[delta_start + t + 1] - vars[delta_start + t], 2));
+      fg[0] += 12500 * (CppAD::pow(vars[delta_start + t + 1] - vars[delta_start + t], 2));
       fg[0] += 10000 * (CppAD::pow(vars[a_start + t + 1] - vars[a_start + t], 2));
     }
 
